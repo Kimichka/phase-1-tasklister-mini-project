503ec4869caf3157ba90bdd8f01375d52249e23b
Mohammad Reza Enteshari <reza.ent@hotmail.com>
2023-04-14 01:07:06 -0400

    Lab Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  let tasklist = []
+  form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let toDo = document.querySelector('#new-task-description')
+  if (toDo.value !== "") {
+  tasklist.push(toDo.value)
+  buildToDo(toDo.value)}
+  form.reset()})
+  function buildToDo(todo) {
+  let p = document.createElement ('p')
+  let btn = document.createElement ('button' )
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  document.querySelector('#list #tasks').appendChild(p)}
+  function handleDelete(e){
+  e.target.parentNode.remove()}});
